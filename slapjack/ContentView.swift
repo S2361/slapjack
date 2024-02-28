737eeb3dbec49c4350a4fdb17d3b80d19584098a
patrickhong <path.plh@gmail.com>
2024-02-28 00:06:49 -0500

    First Commit.

--- slapjack/ContentView.swift
+++ slapjack/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hi qt")
+            Text("Hi Im commiting from Patrick branch")
         }
         .padding()
     }
