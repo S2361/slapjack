cc2dbe6ef68713314f2296548d15a6f611be2358
Suhani <suhanigarima@gmail.com>
2024-02-27 23:49:45 -0500

    Initial Commit

---
+++ slapjack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  slapjack
+//
+//  Created by Suhani Agrawal on 2/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ slapjack/slapjackApp.swift
@@ -0,0 +1,17 @@
+//
+//  slapjackApp.swift
+//  slapjack
+//
+//  Created by Suhani Agrawal on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct slapjackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
