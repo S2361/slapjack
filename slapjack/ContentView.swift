83608701898c4a81b8c825de4c14fe5fd07d970b
patrickhong <path.plh@gmail.com>
2024-02-29 00:46:33 -0500

    Image and bgColor

--- slapjack/ContentView.swift
+++ slapjack/ContentView.swift
@@ -10,13 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+            Image("c8") // Replace "yourImageName" with the actual name from the asset catalog
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
             Text("Hi Im commiting from Patrick branch")
         }
         .padding()
     }
+    
 }
 
 #Preview {
