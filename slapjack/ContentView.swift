68219d9cb42543f6cbee44ebf755e8e9690cdc82
patrickhong <path.plh@gmail.com>
2024-02-29 15:46:33 -0500

    Card images

--- slapjack/ContentView.swift
+++ slapjack/ContentView.swift
@@ -7,15 +7,23 @@
 
 import SwiftUI
 
+let height_scale = 1.45
+let width_size = 100.0
+let bgColor = Color(red: 11/255.0, green: 110/255.0, blue: 26/255.0, opacity: 1.0) // Define a custom color
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image("c8") // Replace "yourImageName" with the actual name from the asset catalog
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-            Text("Hi Im commiting from Patrick branch")
+        ZStack {
+            bgColor.ignoresSafeArea()
+            Image("2_of_diamonds.png")
+            VStack {
+                // 1 : 1.45
+                Image("2_of_clubs") // Replace "yourImageName" with the actual name from the asset catalog
+                    .resizable()
+                    .frame(width: width_size, height: width_size * height_scale)
+                Text("Hi Im commiting from Patrick branch")
+            }
         }
-        .padding()
     }
     
 }
